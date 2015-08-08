43ddc7d9e849bad479562560e0e9d24897b609a6
pstanits <stani078@umn.edu>
2015-08-08 15:06:14 -0500

    Code Changed for faster Computations, LE bug fixed

--- OneNN.m
+++ OneNN.m
@@ -16,43 +16,63 @@ load('C:\Users\nikos\Desktop\data\Labels_test.mat');
 
 %% Split the data into training and testing
 accuracy = zeros(5,1);
-for cases = 1:5
+cases = 2;
+% for cases = 1:5
     train_percent = cases * 0.1;
     indices = 1:1:10000;
-    ind_train = randsample(indices,ceil(train_percent * size(Layer2,1)));
+    ind_train = randsample(indices,ceil(train_percent * size(Layer1,1)));
     ind_test = setdiff(indices,ind_train);
 
-    Cov_train = Layer2(ind_train,:,:);
-    Cov_test = Layer2(ind_test,:,:);
+    Cov_train = Layer1(ind_train,:,:);
+    Cov_test = Layer1(ind_test,:,:);
     labtrain = labels(ind_train);
     labtest = labels(ind_test);
 
 
-
-
-    %% Compute distances between covariances
-    Cov_Distance = zeros(size(Cov_test,1),size(Cov_train,1));
-    index_match = zeros(size(Cov_test,1),1);
-    for i = 1:size(Cov_test,1)
-        disp([i,cases])
-        C1 = reshape(Cov_test(i,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
-        for j = 1:size(Cov_train,1)
-            C2 = reshape(Cov_train(j,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
-            Cov_Distance(i,j) = logEucl(C1,C2);
-        end
-        [~,index_match(i)] = min(Cov_Distance(i,:)); 
-
+    % Offline logarithm computations
+    for i = 1:size(Cov_train,1)
+        Cov_train(i,:,:) = logm(squeeze(Cov_train(i,:,:)) + eps * eye(size(Cov_train,2)));
     end
+    disp('Cov_train logm computed...')
+    for i = 1:size(Cov_test,1)
+        Cov_test(i,:,:) = logm(squeeze(Cov_test(i,:,:)) + eps * eye(size(Cov_train,2)));
+    end
+    disp('Cov_test logm computed...')
+    
+    % Reshape covariances
+    Cov_train = reshape(Cov_train,[(size(Layer1,2))^2,size(Cov_train,1)])';
+    Cov_test = reshape(Cov_test,[(size(Layer1,2))^2,size(Cov_test,1)])';
+    
+    
+    % Distance computation
+    dist_matrix = pdist2(Cov_test, Cov_train, 'euclidean');
+    disp('Distance calculated...')
+    % Nearest neighboe
+    [~, index_match] = min(dist_matrix, [], 2); 
+    disp('Nearest Neighbors retrieved...')
+%     %% Compute distances between covariances
+%     Cov_Distance = zeros(size(Cov_test,1),size(Cov_train,1));
+%     index_match = zeros(size(Cov_test,1),1);
+%     for i = 1:size(Cov_test,1)
+%         disp([i,cases])
+%         C1 = reshape(Cov_test(i,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
+%         for j = 1:size(Cov_train,1)
+%             C2 = reshape(Cov_train(j,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
+%             Cov_Distance(i,j) = logEucl(C1,C2);
+%         end
+%         [~,index_match(i)] = min(Cov_Distance(i,:)); 
+% 
+%     end
 
 
     %% Error Computation 
     error_buffer = 0;
-    for i = 1:size(index_match,1)
+    for i = 1:length(index_match)
         if labtest(i) ~= labtrain(index_match(i))
             error_buffer = error_buffer + 1;
         end
     end
-    accuracy(cases) = (size(index_match,1) - error_buffer) / size(index_match,1);
-end
+    accuracy = (size(index_match,1) - error_buffer) / size(index_match,1);
+% end
 
-save AccuracyLayer2LogEucl.mat accuracy
+% save AccuracyLayer1LogEucl.mat accuracy
--- logEucl.m
+++ logEucl.m
@@ -2,6 +2,6 @@ function distance = logEucl( C1,C2 )
 % Log Euclidean Distance between matrices
 % Panagiotis Stanitsas
 % University of Minnesota
-distance = norm((log(C1) - log(C2)),'fro');
+distance = norm((logm(C1) - logm(C2)),'fro');
 end
 
