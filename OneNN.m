29035c24d2c30cc077baa1e5a8403f8dddfd1d27
pstanits <stani078@umn.edu>
2015-08-11 21:38:15 -0500

    Experimental Structure Improvement

--- OneNN.m
+++ OneNN.m
@@ -17,64 +17,66 @@ load('C:\Users\nikos\Desktop\data\Covariance3.mat');
 load('C:\Users\nikos\Desktop\data\Labels_test.mat');
 load('C:\Users\nikos\Desktop\data\BaselineCov.mat');
 
-
+variables = {Layer1,Layer2,Layer3,Cov_Baseline};
 %% Split the data into training and testing
-accuracy = zeros(5,1);
+accuracy = zeros(5,4);
 for cases = 1:5
     train_percent = cases * 0.1;
     indices = randsample(1:1:10000,2000);
     ind_train = randsample(indices,ceil(train_percent * 2000));
     ind_test = setdiff(indices,ind_train);
+    for vr = 1:length(variables);
+        Dum = variables{vr};
+        Cov_train = Dum(ind_train,:,:);
+        Cov_test = Dum(ind_test,:,:);
+        labtrain = labels(ind_train);
+        labtest = labels(ind_test);
 
-    Cov_train = Layer3(ind_train,:,:);
-    Cov_test = Layer3(ind_test,:,:);
-    labtrain = labels(ind_train);
-    labtest = labels(ind_test);
 
-
-%     Offline logarithm computations
-%     for i = 1:size(Cov_train,1)
-%         Cov_train(i,:,:) = logm(squeeze(Cov_train(i,:,:)) + eps * eye(size(Cov_train,2)));
-%     end
-%     disp('Cov_train logm computed...')
-%     for i = 1:size(Cov_test,1)
-%         Cov_test(i,:,:) = logm(squeeze(Cov_test(i,:,:)) + eps * eye(size(Cov_train,2)));
-%     end
-%     disp('Cov_test logm computed...')
-%     
-%     Reshape covariances
-%     Cov_train = reshape(Cov_train,[(size(Layer3,2))^2,size(Cov_train,1)])';
-%     Cov_test = reshape(Cov_test,[(size(Layer3,2))^2,size(Cov_test,1)])';
-%     
-%     
-%     Distance computation
-%     dist_matrix = pdist2(Cov_test, Cov_train, 'euclidean');
-%     disp('Distance calculated...')
-% %     Nearest neighboe
-%     [~, index_match] = min(dist_matrix, [], 2); 
-%     disp('Nearest Neighbors retrieved...')
-    %% Compute distances between covariances
-    Cov_Distance = zeros(size(Cov_test,1),size(Cov_train,1));
-    index_match = zeros(size(Cov_test,1),1);
-    for i = 1:size(Cov_test,1)
-        disp([i,cases])
-        C1 = reshape(Cov_test(i,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
-        for j = 1:size(Cov_train,1)
-            C2 = reshape(Cov_train(j,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
-            Cov_Distance(i,j) = Stein(C1,C2);
+    %     Offline logarithm computations
+    %     for i = 1:size(Cov_train,1)
+    %         Cov_train(i,:,:) = logm(squeeze(Cov_train(i,:,:)) + eps * eye(size(Cov_train,2)));
+    %     end
+    %     disp('Cov_train logm computed...')
+    %     for i = 1:size(Cov_test,1)
+    %         Cov_test(i,:,:) = logm(squeeze(Cov_test(i,:,:)) + eps * eye(size(Cov_train,2)));
+    %     end
+    %     disp('Cov_test logm computed...')
+    %     
+    %     Reshape covariances
+    %     Cov_train = reshape(Cov_train,[(size(Layer3,2))^2,size(Cov_train,1)])';
+    %     Cov_test = reshape(Cov_test,[(size(Layer3,2))^2,size(Cov_test,1)])';
+    %     
+    %     
+    %     Distance computation
+    %     dist_matrix = pdist2(Cov_test, Cov_train, 'euclidean');
+    %     disp('Distance calculated...')
+    % %     Nearest neighboe
+    %     [~, index_match] = min(dist_matrix, [], 2); 
+    %     disp('Nearest Neighbors retrieved...')
+        %% Compute distances between covariances
+        Cov_Distance = zeros(size(Cov_test,1),size(Cov_train,1));
+        index_match = zeros(size(Cov_test,1),1);
+        for i = 1:size(Cov_test,1)
+            disp([i,cases,vr])
+            C1 = reshape(Cov_test(i,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
+            for j = 1:size(Cov_train,1)
+                C2 = reshape(Cov_train(j,:,:),[size(Cov_train,3),size(Cov_train, 3)]);
+                Cov_Distance(i,j) = Stein(C1,C2);
+            end
+            [~,index_match(i)] = min(Cov_Distance(i,:)); 
         end
-        [~,index_match(i)] = min(Cov_Distance(i,:)); 
-    end
 
 
-    %% Error Computation 
-    error_buffer = 0;
-    for i = 1:length(labtest)
-        if labtest(i) ~= labtrain(index_match(i))
-            error_buffer = error_buffer + 1;
+        %% Error Computation 
+        error_buffer = 0;
+        for i = 1:length(labtest)
+            if labtest(i) ~= labtrain(index_match(i))
+                error_buffer = error_buffer + 1;
+            end
         end
+        accuracy(cases,vr) = (size(index_match,1) - error_buffer) / size(index_match,1);
     end
-    accuracy(cases) = (size(index_match,1) - error_buffer) / size(index_match,1);
 end
 
-save AccuracyLayer3Stein.mat accuracy
+save AccuracyStein.mat accuracy
