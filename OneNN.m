3622d88a1a0ebbe3823adf4a5bd38bc8e6b95137
pstanits <stani078@umn.edu>
2015-08-11 21:13:05 -0500

    Stein Comparison on 2000 samples added

--- OneNN.m
+++ OneNN.m
@@ -7,29 +7,32 @@
 clear all
 clc
 
+
+stream = RandStream.getGlobalStream;
+reset(stream);
+
 load('C:\Users\nikos\Desktop\data\Covariance1.mat');
 load('C:\Users\nikos\Desktop\data\Covariance2.mat');
 load('C:\Users\nikos\Desktop\data\Covariance3.mat');
 load('C:\Users\nikos\Desktop\data\Labels_test.mat');
-
+load('C:\Users\nikos\Desktop\data\BaselineCov.mat');
 
 
 %% Split the data into training and testing
 accuracy = zeros(5,1);
-cases = 1;
-% for cases = 1:5
+for cases = 1:5
     train_percent = cases * 0.1;
-    indices = 1:1:10000;
-    ind_train = randsample(indices,ceil(train_percent * size(Layer2,1)));
+    indices = randsample(1:1:10000,2000);
+    ind_train = randsample(indices,ceil(train_percent * 2000));
     ind_test = setdiff(indices,ind_train);
 
-    Cov_train = Layer2(ind_train,:,:);
-    Cov_test = Layer2(ind_test,:,:);
+    Cov_train = Layer3(ind_train,:,:);
+    Cov_test = Layer3(ind_test,:,:);
     labtrain = labels(ind_train);
     labtest = labels(ind_test);
 
 
-%     % Offline logarithm computations
+%     Offline logarithm computations
 %     for i = 1:size(Cov_train,1)
 %         Cov_train(i,:,:) = logm(squeeze(Cov_train(i,:,:)) + eps * eye(size(Cov_train,2)));
 %     end
@@ -39,15 +42,15 @@ cases = 1;
 %     end
 %     disp('Cov_test logm computed...')
 %     
-%     % Reshape covariances
-%     Cov_train = reshape(Cov_train,[(size(Layer2,2))^2,size(Cov_train,1)])';
-%     Cov_test = reshape(Cov_test,[(size(Layer2,2))^2,size(Cov_test,1)])';
+%     Reshape covariances
+%     Cov_train = reshape(Cov_train,[(size(Layer3,2))^2,size(Cov_train,1)])';
+%     Cov_test = reshape(Cov_test,[(size(Layer3,2))^2,size(Cov_test,1)])';
 %     
 %     
-%     % Distance computation
+%     Distance computation
 %     dist_matrix = pdist2(Cov_test, Cov_train, 'euclidean');
 %     disp('Distance calculated...')
-%     % Nearest neighboe
+% %     Nearest neighboe
 %     [~, index_match] = min(dist_matrix, [], 2); 
 %     disp('Nearest Neighbors retrieved...')
     %% Compute distances between covariances
@@ -61,18 +64,17 @@ cases = 1;
             Cov_Distance(i,j) = Stein(C1,C2);
         end
         [~,index_match(i)] = min(Cov_Distance(i,:)); 
-
     end
 
 
     %% Error Computation 
     error_buffer = 0;
-    for i = 1:200
+    for i = 1:length(labtest)
         if labtest(i) ~= labtrain(index_match(i))
             error_buffer = error_buffer + 1;
         end
     end
-    accuracy = (size(index_match,1) - error_buffer) / size(index_match,1);
-% end
+    accuracy(cases) = (size(index_match,1) - error_buffer) / size(index_match,1);
+end
 
-% save AccuracyLayer2LogEucl.mat accuracy
+save AccuracyLayer3Stein.mat accuracy
