bb6ea285fea17f81d52418addf36dd7ff55d3985
pstanits <stani078@umn.edu>
2015-08-13 17:58:43 -0500

    New Code for Experiments Initiation

--- OneNN.m
+++ OneNN.m
@@ -8,8 +8,8 @@ clear all
 clc
 
 
-stream = RandStream.getGlobalStream;
-reset(stream);
+% stream = RandStream(6387);
+% reset(stream);
 
 load('C:\Users\nikos\Desktop\data\Covariance1.mat');
 load('C:\Users\nikos\Desktop\data\Covariance2.mat');
@@ -18,9 +18,11 @@ load('C:\Users\nikos\Desktop\data\Labels_test.mat');
 load('C:\Users\nikos\Desktop\data\BaselineCov.mat');
 
 variables = {Layer1,Layer2,Layer3,Cov_Baseline};
+% variables = {Cov_Baseline};
 %% Split the data into training and testing
 accuracy = zeros(5,4);
 for cases = 1:5
+    stream = RandStream('twister','Seed',0);
     train_percent = cases * 0.1;
     indices = randsample(1:1:10000,2000);
     ind_train = randsample(indices,ceil(train_percent * 2000));
@@ -33,27 +35,27 @@ for cases = 1:5
         labtest = labels(ind_test);
 
 
-    %     Offline logarithm computations
-    %     for i = 1:size(Cov_train,1)
-    %         Cov_train(i,:,:) = logm(squeeze(Cov_train(i,:,:)) + eps * eye(size(Cov_train,2)));
-    %     end
-    %     disp('Cov_train logm computed...')
-    %     for i = 1:size(Cov_test,1)
-    %         Cov_test(i,:,:) = logm(squeeze(Cov_test(i,:,:)) + eps * eye(size(Cov_train,2)));
-    %     end
-    %     disp('Cov_test logm computed...')
-    %     
-    %     Reshape covariances
-    %     Cov_train = reshape(Cov_train,[(size(Layer3,2))^2,size(Cov_train,1)])';
-    %     Cov_test = reshape(Cov_test,[(size(Layer3,2))^2,size(Cov_test,1)])';
-    %     
-    %     
-    %     Distance computation
-    %     dist_matrix = pdist2(Cov_test, Cov_train, 'euclidean');
-    %     disp('Distance calculated...')
-    % %     Nearest neighboe
-    %     [~, index_match] = min(dist_matrix, [], 2); 
-    %     disp('Nearest Neighbors retrieved...')
+%     %     Offline logarithm computations
+%         for i = 1:size(Cov_train,1)
+%             Cov_train(i,:,:) = my_logm(squeeze(Cov_train(i,:,:)));
+%         end
+%         disp('Cov_train logm computed...')
+%         for i = 1:size(Cov_test,1)
+%             Cov_test(i,:,:) = my_logm(squeeze(Cov_test(i,:,:)));
+%         end
+%         disp('Cov_test logm computed...')
+%         
+% %         Reshape covariances
+%         Cov_train = reshape(Cov_train,[(size(Dum,2))^2,size(Cov_train,1)])';
+%         Cov_test = reshape(Cov_test,[(size(Dum,2))^2,size(Cov_test,1)])';
+%         
+%         
+% %         Distance computation
+%         dist_matrix = pdist2(Cov_test, Cov_train, 'euclidean');
+%         disp('Distance calculated...')
+%     %     Nearest neighboe
+%         [~, index_match] = min(dist_matrix, [], 2); 
+%         disp('Nearest Neighbors retrieved...')
         %% Compute distances between covariances
         Cov_Distance = zeros(size(Cov_test,1),size(Cov_train,1));
         index_match = zeros(size(Cov_test,1),1);
@@ -67,8 +69,8 @@ for cases = 1:5
             [~,index_match(i)] = min(Cov_Distance(i,:)); 
         end
 
-
-        %% Error Computation 
+% 
+%         %% Error Computation 
         error_buffer = 0;
         for i = 1:length(labtest)
             if labtest(i) ~= labtrain(index_match(i))
@@ -79,4 +81,4 @@ for cases = 1:5
     end
 end
 
-save AccuracyStein.mat accuracy
+% save AccuracyStein.mat accuracy
